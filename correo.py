2cc1a3e61883f1f06be29d6b9a331b96c39db592
JoseZapardiel <Jose.Zapardiel@alu.uclm.es>
2020-10-12 22:44:45 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,2 +1,5 @@
-my_email='Jose.Zapardiel@alu.uclm.es'
+# Define aqui mismo la fundion my_email sin argumentos que
+# devuelve tu correo electrónico de la ULCM
 
+def my_email():
+    return 'Jose.Zapardiel@alu.uclm.es'
